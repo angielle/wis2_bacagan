e18f759e63c2b70f09280dabe638af5ade483733
angielle <dannahangielle@gmail.com>
2019-05-12 09:47:42 +0800

    Add basic styling

---
+++ puzzle/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="css/style.css" />
+    </head>
+    <body>
+        <table>
+            <tr id="row1">
+                <td>
+                    <button>A</button>
+                </td>
+                <td>
+                    <button>B</button>
+                </td>
+                <td>
+                    <button>C</button>
+                </td>
+            </tr>
+            <tr id="row2">
+                <td>
+                    <button>D</button>
+                </td>
+                <td>
+                    <button>E</button>
+                </td>
+                <td>
+                    <button>F</button>
+                </td>
+            </tr>
+            <tr id="row3">
+                <td>
+                    <button>G</button>
+                </td>
+                <td>
+                    <button>H</button>
+                </td>
+                <td>
+                    <button>I</button>
+                </td>
+            </tr>
+        </table>
+
+    </body>
+    <script src="js/script.js"></script>
+</html>
