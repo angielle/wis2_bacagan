7033d21d64fcdef4a15012ed446794c22d6b9867
M306User <M306User@M306.uc-bcf.edu.ph>
2019-05-18 19:07:44 +0800

    [Puzzle] Remove animation

--- puzzle/index.php
+++ puzzle/index.php
@@ -2,43 +2,27 @@
 <html>
 	<head>
 		<title>Puzzle</title>
+		<link rel="stylesheet" href="css/animate.css" crossorigin="anonymous">	
+		<link rel="stylesheet" href="css/style.css" crossorigin="anonymous">	
 		<style>
-		* {
-			padding: 0;
-			margin: 0;
-		}
-		.grid {
-			display: grid;
-			grid-template: 1fr 1fr 1fr/1fr 1fr 1fr;
-			grid-gap: 10px;
-			height: 100vh;
-		}
-
-		.box {
-			padding-top: 20px;
-			font-family: 'Century Gothic';
-			color: white;
-			font-size: 20vh;
-			text-align: center;
-			box-shadow: 2px 2px 2px black;
-		}
+		
 	</head>
 	
 	</style>
 	<body>
 		<div class="grid">
-			<div class="box" id="1" style="background-color: black">A</div>
-			<div class="box" id="2" style="background-color: red">B</div>
-			<div class="box" id="3" style="background-color: orange">C</div>
-			<div class="box" id="4" style="background-color: yellow; color: black">D</div>
-			<div class="box" id="5" style="background-color: green">E</div>
-			<div class="box" id="6" style="background-color: blue">F</div>
-			<div class="box" id="7" style="background-color: indigo">G</div>
-			<div class="box" id="8" style="background-color: violet">H</div>
-			<div class="box" id="9" style="background-color: white; color: black">I</div>
+			<div class="box 1c" style="background-color: black">A</div>
+			<div class="box 2c" style="background-color: red">B</div>
+			<div class="box 3c" style="background-color: orange">C</div>
+			<div class="box 1c" style="background-color: yellow; color: black">D</div>
+			<div class="box 2c" style="background-color: green">E</div>
+			<div class="box 3c" style="background-color: blue">F</div>
+			<div class="box 1c" style="background-color: indigo">G</div>
+			<div class="box 2c" style="background-color: violet">H</div>
+			<div class="box 3c" style="background-color: white; color: black">I</div>
 		</div>
     </body>
+    <script type="text/javascript" src="js/jquery-min.js"></script>
     <script type="text/javascript" src="js/jquery.js"></script>
-    <script type="text/javascript" src="js/changed.js"></script>
     
 </html>
